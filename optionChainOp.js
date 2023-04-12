8d9067fc2bed5fdb83862f2c9d6a9f6e9294dd83
IbadDoesCode <122964438+IbadDoesCode@users.noreply.github.com>
2023-04-11 19:59:19 -0700

    Add files via upload

---
+++ optionChainOp.js
@@ -0,0 +1,14 @@
+// Optional Chaining Operator
+
+const student = {
+    name: "Jack",
+    age: 21,
+    address: {
+        city: "NYC"
+    }
+}
+const deptName = student.dept.name
+console.log(deptName)
+
+
+// ? helps stop throwing error.
